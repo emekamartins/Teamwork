1c33fd6189f7a1efee6f6457732986377f3bb063
emekamartins <emartinsokwor@gmail.com>
2019-11-21 07:48:12 +0100

    travis failed test fixes 20

--- src/db/database.js
+++ src/db/database.js
@@ -2,9 +2,9 @@
 const { Pool } = require('pg');
 
 const isProduction = process.env.NODE_ENV === 'production';
-const isTest = process.env.NODE_ENV === 'test';
 
-const connectionString = isTest ? `postgres://${process.env.ELEPHANTDB_USER}:${process.env.ELEPHANTDB_PASS}@${process.env.ELEPHANTDB_HOST}:${process.env.ELEPHANTDB_PORT}/${process.env.ELEPHANTDB_DATABASE}` :  `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+const connectionString = `postgres://${process.env.ELEPHANTDB_USER}:${process.env.ELEPHANTDB_PASS}@${process.env.ELEPHANTDB_HOST}:${process.env.ELEPHANTDB_PORT}/${process.env.ELEPHANTDB_DATABASE}`
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
