bbf47d5b5cfef0c65a93432ed5e8151ac065bfaa
emekamartins <emartinsokwor@gmail.com>
2019-11-22 11:17:44 +0100

    create admin user 3

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,7 +7,7 @@ const Admin = require('../middleware/isAdmin');
 
 
 // Routes for users
-router.post('/create-user', userController.addUser);
+router.post('/create-user', Admin.verifyAdminToken, userController.addUser);
 router.post('/signin', userController.loginUser);
 router.post('/logout', Auth.verifyToken, userController.logoutUser);
 router.get('/me', Auth.verifyToken, userController.getUser);
