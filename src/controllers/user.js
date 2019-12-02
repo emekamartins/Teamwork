c8f4b54063ab9104dc1c05a17d272e285a9b82d6
emekamartins <emartinsokwor@gmail.com>
2019-12-02 07:30:46 +0100

    email validation error fix

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -66,12 +66,12 @@ exports.addUser = async (request, response) => {
         userId: rows[0].team_id,
       },
     });
-  } catch (err) {
+  } catch (e) {
     if (e.routine === '_bt_check_unique') {
       return response.status(500).send({ status: 'error', error: 'email already exist' });
     }
 
-    return response.status(500).send({ status: 'error', error: err });
+    return response.status(500).send({ status: 'error', error: e });
   }
 };
 
